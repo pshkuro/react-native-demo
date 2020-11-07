37c008610eafd61051dce983c655d88c312513f5
Polina Shkuro <60005952+pshkuro@users.noreply.github.com>
2020-11-07 14:44:52 +0300

    feat(App): reducer

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,62 +1,66 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { StyleSheet, View, Alert } from 'react-native';
 
 import { THEME } from './theme';
 import {Navbar} from './components/Navbar';
 import {MainScreen} from './screens/MainScreen';
 import {TodoScreen} from './screens/TodoScreen';
+import { TodoContext } from './context/todo/todoContext';
 
 
 export const MainLayout = () => {
+    // Получили контекст который ранее создали
+    const {todos, addTodo, updateTodo, removeTodo} = useContext(TodoContext);
+     
     const [todoId, setTodoId] = useState(null);
-    const [todos, setTodos] = useState([]);
+    // const [todos, setTodos] = useState([]);
 
-    const handleChangetextfield = (title) => {
-        setTodos(prevState => [...prevState, {id: Date.now().toString(), title}]);
-      }
+    // const handleChangetextfield = (title) => {
+    //     setTodos(prevState => [...prevState, {id: Date.now().toString(), title}]);
+    //   }
     
-      const updateTodo = (id, title) => {
-        setTodos(prevState => prevState.map(todo => {
-          if (todo.id === id) {
-            todo.title = title;
-          }
-          return todo;
-        }))
-      }
+    //   const updateTodo = (id, title) => {
+    //     setTodos(prevState => prevState.map(todo => {
+    //       if (todo.id === id) {
+    //         todo.title = title;
+    //       }
+    //       return todo;
+    //     }))
+    //   }
     
-      const handleTodoLongPress = (id) => {
-        const todo = todos.find(item => item.id);
-        Alert.alert(
-          'Удаление элемента',
-          `Вы уверены, что хотите удалить "${todo.title}"`,
-          [
-            {
-              text: 'Отмена',
-              style: 'cancel'
-            },
-            { text: 'Удалить',
-              style: 'destructive',
-              onPress: () => {
-                setTodoId(null);
-                setTodos(prevState => prevState.filter(todo => todo.id !== id));
-              } 
-            }
-          ],
-          { cancelable: false }
-        );
+    //   const handleTodoLongPress = (id) => {
+    //     const todo = todos.find(item => item.id);
+    //     Alert.alert(
+    //       'Удаление элемента',
+    //       `Вы уверены, что хотите удалить "${todo.title}"`,
+    //       [
+    //         {
+    //           text: 'Отмена',
+    //           style: 'cancel'
+    //         },
+    //         { text: 'Удалить', 
+    //           style: 'destructive',
+    //           onPress: () => {
+    //             setTodoId(null);
+    //             setTodos(prevState => prevState.filter(todo => todo.id !== id));
+    //           } 
+    //         }
+    //       ],
+    //       { cancelable: false }
+    //     );
         
-      }
+    //   }
 
     let content = <MainScreen 
         todos={todos}
-        handleChangetextfield={handleChangetextfield}
-        handleTodoLongPress={handleTodoLongPress}
+        handleChangetextfield={addTodo}
+        handleTodoLongPress={removeTodo}
         openTodo={setTodoId}
     />;
 
     if (todoId) {
         const todo = todos.find(item => item.id === todoId);
-        content = <TodoScreen goBack={() => setTodoId(null)} todo={todo} removeTodo={handleTodoLongPress} onSave={updateTodo}/>
+        content = <TodoScreen goBack={() => setTodoId(null)} todo={todo} removeTodo={removeTodo} onSave={updateTodo}/>
     }
 
     return (
--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -1,4 +1,5 @@
 import React, {useReducer} from 'react';
+import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types';
 import {TodoContext} from './todoContext';
 import {todoReducer} from './todoReducer';
 
@@ -10,8 +11,19 @@ export const TodoState = ({children}) => {
     // const [state, dispatch] = useReducer(reducer, initialArg, init);
     const [state, dispatch] = useReducer(todoReducer, initialArg);
 
+
+    // Это просто action creators
+    const addTodo = title => dispatch({type: ADD_TODO, title});
+
+    const removeTodo = id => dispatch({type: REMOVE_TODO, id});
+
+    const updateTodo = (id, title) => dispatch({type: UPDATE_TODO, id, title});
+
     // Provider позволяет дочерним компонентам подписаться на изменения Context
     return <TodoContext.Provider value={{
         todos: state.todos,
+        addTodo,
+        removeTodo,
+        updateTodo
     }}>{children}</TodoContext.Provider>
 }
